52b97c9a9f1ce7d137934dda116b368117d84ece
cborre <cborre2007@gmail.com>
2016-04-30 21:20:11 -0430

    Hola
    Ejemplo de Código AngularJS

---
+++ app/scripts/app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name holaApp
+ * @description
+ * # holaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('holaApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
